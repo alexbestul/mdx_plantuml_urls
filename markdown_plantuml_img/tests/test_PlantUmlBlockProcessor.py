073b8496bd80123cbc7fb36e08afa5e1f98eaf18
alexbestul <alexbestul@users.noreply.github.com>
2016-09-02 22:20:57 -0500

    Added first actual unit test.

--- markdown_plantuml_img/tests/test_PlantUmlBlockProcessor.py
+++ markdown_plantuml_img/tests/test_PlantUmlBlockProcessor.py
@@ -1,35 +1,14 @@
 import mock
 import pytest
 from pytest_mock import mocker
-import plantuml
-from markdown_plantuml_img import PlantUmlUrlExtension
-import markdown
-import textwrap
+from markdown_plantuml_img import PlantUmlBlockProcessor
 
-def test_happypath(mocker):
-    # Given the simplest possible PlantUML input,
-    # show that the URL generator is called, and
-    # that the URL it returns is inserted into the
-    # resulting document.  With this behavior
-    # established, other tests may focus on asserting
-    # that PlantUML.get_url is passed the correct text.
-    #
-    # This test does rely on the default PlantUML server
-    # URL, as provided by the plantuml package.  If that
-    # default ever changes, the test will fail.
-
+def test_whenblockexactlymatchesstartumltoken_testreturnstrue(mocker):
     # Arrange
-    #mocker.patch('plantuml.PlantUML.get_url')
-    #plantuml.PlantUML.get_url.retur_value = 'xxbarxx'
-
-    text = textwrap.dedent('''\
-    @startuml
-    @enduml
-    ''')
+    bp = PlantUmlBlockProcessor(mock.Mock())
 
     # Act
-    output = markdown.markdown(text, extensions=[PlantUmlUrlExtension()])
+    result = bp.test(None, '@startuml')
 
-    #Assert
-    #plantuml.PlantUML.get_url.assert_called_with(text)
-    assert output == '<img src="http://www.plantuml.com/plantuml/img/SoWkIImgAStDuN98pKi1qG00" />'
+    # Assert
+    assert result == True
---
+++ markdown_plantuml_img/tests/test_integration.py
@@ -0,0 +1,28 @@
+import pytest
+from markdown_plantuml_img import PlantUmlUrlExtension
+import markdown
+import textwrap
+
+def test_happypath():
+    # Given the simplest possible PlantUML input,
+    # show that the URL generator is called, and
+    # that the URL it returns is inserted into the
+    # resulting document.  With this behavior
+    # established, other tests may focus on asserting
+    # that PlantUML.get_url is passed the correct text.
+    #
+    # This test does rely on the default PlantUML server
+    # URL, as provided by the plantuml package.  If that
+    # default ever changes, the test will fail.
+
+    # Arrange
+    text = textwrap.dedent('''\
+        @startuml
+        @enduml
+    ''')
+
+    # Act
+    output = markdown.markdown(text, extensions=[PlantUmlUrlExtension()])
+
+    #Assert
+    assert output == '<img src="http://www.plantuml.com/plantuml/img/SoWkIImgAStDuN98pKi1qG00" />'
