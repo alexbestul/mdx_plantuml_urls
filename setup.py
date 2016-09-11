5feec220d2b268f2e3e24d831a1d02a73bafa3bf
alexbestul <alexbestul@users.noreply.github.com>
2016-09-11 12:58:33 -0500

    Add test support for setup.py.
    Run tests with the command 'python setup.py test'.

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+# Helper class for binding the 'setup.py test' command to pytest.
+# Taken from the pytest docs:  http://doc.pytest.org/en/latest/goodpractices.html#manual-integration
+class PyTest(TestCommand):
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='mdx_plantuml_urls',
-    version='0.1'
+    version='0.1',
+    setup_requires=['plantuml', 'markdown'],
+    tests_require=['pytest', 'mock', 'pytest-mock'],
+    test_suite = "not None",  # Without it, run_tests is not called.
+    cmdclass = {'test': PyTest},
 )
