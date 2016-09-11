fd309806a5e80af806d98e8c0a483bc949330e48
alexbestul <alexbestul@users.noreply.github.com>
2016-09-11 11:31:27 -0500

    Clean up setup.py.
    Fix name, and remove some lines that I don't think are important.

--- mdx_plantuml_urls/setup.py
+++ mdx_plantuml_urls/setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
 
 setup(
-    name='mdx_plantuml_img',
-    version='0.1',
-    py_modules=['mdx_plantuml_img'],
-    install_requires=['markdown>=2.5'],
+    name='mdx_plantuml_urls',
+    version='0.1'
 )
